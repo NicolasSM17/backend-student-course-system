3af7d8ea32da26d74b790deb2df8163830e14c85
NicolasSM17 <segoviamatosjesunicolas@gmail.com>
2023-10-16 16:47:09 -0500

    subida a github

---
+++ SpringMicroservices/microservice-course/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO courses (name, teacher) VALUES ('Matematicas', 'Profesor A');
+INSERT INTO courses (name, teacher) VALUES ('Historia', 'Profesor B');
+INSERT INTO courses (name, teacher) VALUES ('Ciencias', 'Profesor C');
+INSERT INTO courses (name, teacher) VALUES ('Literatura', 'Profesor D');
+INSERT INTO courses (name, teacher) VALUES ('Educacion Fisica', 'Profesor E');
---
+++ SpringMicroservices/microservice-student/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO students (name, last_name, email, course_id) VALUES ('Juan', 'Perez', 'juan.perez@example.com', 1);
+INSERT INTO students (name, last_name, email, course_id) VALUES ('Maria', 'Gonzalez', 'maria.gonzales@example.com', 2);
+INSERT INTO students (name, last_name, email, course_id) VALUES ('Pedro', 'Lopez', 'pedro.lopez@example.com', 1);
+INSERT INTO students (name, last_name, email, course_id) VALUES ('Ana', 'Martinez', 'ana.martinez@example.com', 3);
+INSERT INTO students (name, last_name, email, course_id) VALUES ('Carlos', 'Sanchez', 'carlos.sanchez@example.com', 3);
